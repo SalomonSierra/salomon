5a8e51ebf69ab7639ca99d4fbdf74e55299c6e3b
FabianS7 <juezsalomon@gmail.com>
2021-10-12 22:50:50 +0000

    actualizado

--- src/private/runtmp/run-contest1-user1.php
+++ src/private/runtmp/run-contest1-user1.php
@@ -1,4 +1,4 @@
-<!-- 1632361484 --> <?php exit; ?>	0	lechuga	33F404
+<!-- 1633967182 --> <?php exit; ?>	0	rojo	FF0000
 <br>
 <table class="table table-hover">
  <thead>
@@ -13,12 +13,57 @@
  </thead>
 <tbody>
  <tr>
-  <td>308030201</td>
-  <td>67</td>
+  <td>663175901</td>
+  <td>3</td>
   <td>A</td>
-  <td>C++11</td>
-  <td class="text-success">YES - Accepted <img alt="lechuga" width="15" src="/salomon/balloons/09f7dde6e1d62020b2a6420ea7f8dcc0.png" /></td>
-<td><a href="../filedownload.php?oid=16560&filename=bUR2THFVZEdRWlhCYlh1U05Kb0E1akFnZGQ3VFQ3U25WWXpWaUNhazcvVTYxeXFxV3Z0VTFxSkVzcWtoR0ZqQklFbTF6bjhVMU5uUUlWbER3TGdNbkJTOXNZLzgzaFBBTFlRR1l5ajk1WTA9&check=4776c3132e43a7885f67803771d7940eaf34138c49ff93348adbba62d48ec96f#toolbar=0">print.cpp</a>&nbsp;&nbsp;<a href="#" class="btn btn-primary" style="font-weight:bold" onClick="window.open('../filewindow.php?oid=16560&filename=YVRhem8rb1BBYzM4WGo0RU5rK1lzOUtnd3IxOHNIV1Qxb210MUhrOG9OeFVDQ21xcko1RFBxSFVyeTQzbzZhWWhIcE1RN1Fnc1orRVRIYlNNb1BtZ0dQbGx0VnZ0VndFWmdub01uUVFreXc9&check=4776c3132e43a7885f67803771d7940eaf34138c49ff93348adbba62d48ec96f#toolbar=0', 'View - SOURCE', 'width=680,height=600,scrollbars=yes,resizable=yes')">Ver Codigo</a>
+  <td>Java</td>
+  <td class="text-success">YES - Accepted <img alt="rojo" width="15" src="/salomon/balloons/0b317cbe1e5d0b6ab93bfa1ab4742302.png" /></td>
+<td><a href="../filedownload.php?oid=16801&filename=cnJMbnM4Y3puZEYvVVl3QXN1Rk1LdXRrcStDL3dnSzhHa3htWGNMb0xPS3BrV21hRW4xQm5UT2VmMzBWSmNHaUxMVFFmcmdlTnFBcUx3ajl1WE53MXpxaG42T0N4dy8ySy9qbTU5K2R5MXc9&check=fe3591488c6608b5f31044ffe8b616773d92c843961230dc6f95b2e594c2ce31#toolbar=0">print.java</a>&nbsp;&nbsp;<a href="#" class="btn btn-primary" style="font-weight:bold" onClick="window.open('../filewindow.php?oid=16801&filename=eUtTVThMU29rNUdrTkZqd3JsSzZGNTFvWitMekplMXJ1Qi9CQWlZQTJWczcxOXhNL0tiVHYxaTVtd3YwYmFVQ2JYRGlMMk04Tk5SM09pV2JGR2RBaVk4OXFld0Z1UXhUU1ZCbGRRTnRYcWc9&check=fe3591488c6608b5f31044ffe8b616773d92c843961230dc6f95b2e594c2ce31#toolbar=0', 'View - SOURCE', 'width=680,height=600,scrollbars=yes,resizable=yes')">Ver Codigo</a>
+</td>
+ </tr>
+ <tr>
+  <td>664822402</td>
+  <td>6</td>
+  <td>A</td>
+  <td>Java</td>
+  <td class="text-danger"><a class="text-danger" href="" onClick="window.open('../filewindow.php?oid=16808&filename=dVo3d2oxN29KbmVSQTZWdnFiVTFvZUV5cnozUHJrVmtPWFJRUWVEdTlTMlg3dHVHWDd1QzhyNXNJb0VQUCtQb01EV1lENjUvZ0R0QnY0VkQ4UlFTNnc9PQ%3D%3D&check=42c5ab108fecd815cb99ab005443489068f8a962d71fce5d90e527caf4eb04a1#toolbar=0', 'View - OUTUSER','width=680,height=600,scrollbars=yes,resizable=yes')">NO - Presentation error</a></td>
+<td><a href="../filedownload.php?oid=16805&filename=MjBwZGxtZzlQbU14N2xTWVJVY3VNN2ZWL0VxOGs3bG1ScCs2T2tNSkhEZnBJUnJwNEFWWkFwUjFFMkVyek5aclJoZ3pDQS9hWE5kZ2tSV0NSdDE4ZmQxckdhNnVtN1o3NW45K2NGblBkV3c9&check=88a28f3273a0d2693e09a46cfcbc55959e7248776320bdafef6bdbdb99dbec07#toolbar=0">print.java</a>&nbsp;&nbsp;<a href="#" class="btn btn-primary" style="font-weight:bold" onClick="window.open('../filewindow.php?oid=16805&filename=NFo0TlFQZ1BnUDExVklEbENielZoSHVZaVduY3BhWWNOek95TjgrckxuYm1ZUld0alJKdkZielFpbTBYUkNaUVhGZzI1SEx4YVVlNWZRZFpvamtISWFTT0FVb0RSQ1RrcXBjdTYxNVBzeUk9&check=88a28f3273a0d2693e09a46cfcbc55959e7248776320bdafef6bdbdb99dbec07#toolbar=0', 'View - SOURCE', 'width=680,height=600,scrollbars=yes,resizable=yes')">Ver Codigo</a>
+</td>
+ </tr>
+ <tr>
+  <td>665933403</td>
+  <td>8</td>
+  <td>A</td>
+  <td>Java</td>
+  <td class="text-danger"><a class="text-danger" href="" onClick="window.open('../filewindow.php?oid=16812&filename=bjI5Rng0a2xnK0EwVnloWXpxRURUZTNGTDlrY0MveW12SWp4c3V5YWJkOTFITU9kNms2ZUhwS0N2MmxPZXlodmhNQjhMS0crVVYvUFRSZW4vMms2SVE9PQ%3D%3D&check=0e9f83564b9f94d237e1f534295490585e69c8a2dcc5c675f59820ebef179f22#toolbar=0', 'View - OUTUSER','width=680,height=600,scrollbars=yes,resizable=yes')">NO - Name mismatch</a></td>
+<td><a href="../filedownload.php?oid=16809&filename=Q05iVnU1Z3k1Q3JpSGNsQnUvV1RTeDhKalNmVDNNZ1BVTHVXQ29tc2ZrRTFFKzUzK1kwZGtjKzNqWE9NTU1rYmZyMjMzN3JwQjZWR25leDcwUUtYNENKN0E0TmhyOXlDV0dLRkJjOEl5WUE9&check=83e25f547ad7d1a06e725601c12fc607d7f93d9fbba1af172b2fdde49b548cb2#toolbar=0">print.java</a>&nbsp;&nbsp;<a href="#" class="btn btn-primary" style="font-weight:bold" onClick="window.open('../filewindow.php?oid=16809&filename=UXJGY3BNTUVPUjcrcHExQUtJMW83QVNXbWlaclEwQkozeDFSd2Z1Qi9maThTNkRKaVNHdEUzbGxJcXJJaVVMTmNSbEMrV3dyMjBEczFCemVmUTF2NzRtVnFIWERGZHVzZHBKVVlhS3VwRnc9&check=83e25f547ad7d1a06e725601c12fc607d7f93d9fbba1af172b2fdde49b548cb2#toolbar=0', 'View - SOURCE', 'width=680,height=600,scrollbars=yes,resizable=yes')">Ver Codigo</a>
+</td>
+ </tr>
+ <tr>
+  <td>669291904</td>
+  <td>14</td>
+  <td>A</td>
+  <td>Java</td>
+  <td class="text-danger"><a class="text-danger" href="" onClick="window.open('../filewindow.php?oid=16816&filename=emZWSk1IeE5FeDNsa000VThEM1JhbVR2c0UzVllFektpNjRSQk0xT1JxaFBicmFuSHRmQjBqQm1PS1BYZGFSMGExNGt1OVo4b21OUkdZdmlRanFjK3c9PQ%3D%3D&check=f9c28fe749482ebead66da290169a9a0410759a46f0144a8b6a48fbd1ca67f37#toolbar=0', 'View - OUTUSER','width=680,height=600,scrollbars=yes,resizable=yes')">NO - Compilation error</a></td>
+<td><a href="../filedownload.php?oid=16813&filename=VHhXSG1UZkFJeXZzSittU2JiT2FlYll1dlNzazdpMk5UMnE4eGNSdnE5V202NCtDY3ZMQm51cGlzUEZGN0FxTTgrTW1uT0Z3UmhLcldlSm15UGpkT3RIU1YyK2JVZkY0K3JiTjJpTTlDejg9&check=107e12995164a1c9d029bd6f71d18f11e6a3f4c32ec09247bd9786523ca6e5bd#toolbar=0">print.java</a>&nbsp;&nbsp;<a href="#" class="btn btn-primary" style="font-weight:bold" onClick="window.open('../filewindow.php?oid=16813&filename=eWxwaUpaMFdHT1R5VGFJNUVUQ3ZvaHE1dXJBUUxQdlZ2U0FuRS9zSnphYnhuYVlKdU54cU1UYVNmOGRYSXMxbmJKN1hQRGF5UXJrcUJBWTEwK0VYdTBIWis0aDZTNGkwbGNuWmRoN0FLK2s9&check=107e12995164a1c9d029bd6f71d18f11e6a3f4c32ec09247bd9786523ca6e5bd#toolbar=0', 'View - SOURCE', 'width=680,height=600,scrollbars=yes,resizable=yes')">Ver Codigo</a>
+</td>
+ </tr>
+ <tr>
+  <td>670245005</td>
+  <td>15</td>
+  <td>A</td>
+  <td>Java</td>
+  <td class="text-danger"><a class="text-danger" href="" onClick="window.open('../filewindow.php?oid=16820&filename=cEdWWmowalJ2UXl6SVMzYmFMaGVZQ0lIVGJlRkRJVDcxSis4bGRiRzd4WjQyR0VHSEFyTGxDbjNGMStsVHVoMzZpRXo3Nlo0ckpRNnhRTHU0bjdsY3c9PQ%3D%3D&check=44663192bb7a6bd51a5d1f636915348323152150b0dc0589ad59e072e3e11fc9#toolbar=0', 'View - OUTUSER','width=680,height=600,scrollbars=yes,resizable=yes')">NO - Name mismatch</a></td>
+<td><a href="../filedownload.php?oid=16817&filename=d3hGK3Exdkp6VkFNbDZqNzVBTk1BZExmRVJ4THkwYS9mNld3NCt0Q2pqZ0FtVlB1RXRiZURIMUxLSVpEeTVnZU40RjQwVUhaZDlHUmRNSy81UG50VkRVOWNIUnUvTVZtYkZxYmVHcWdtTk09&check=d0315bb04a6476b608b23064bf1fdd6929d39f18349fc9e972167a6eed8e4aeb#toolbar=0">suma.java</a>&nbsp;&nbsp;<a href="#" class="btn btn-primary" style="font-weight:bold" onClick="window.open('../filewindow.php?oid=16817&filename=MGdKeU1TQUhQTWRzR3VuakdlRlRYT2IyQlhaZWp0eWJvUHRKcUxRR2tpK3EreGpLZk8xZGIybld3WjMrTFFQdW01UHlmbHZraVI0QWdMTEEzWkdzL1lpOTY3b1R3TitXbkxjSTd5WmtTN2s9&check=d0315bb04a6476b608b23064bf1fdd6929d39f18349fc9e972167a6eed8e4aeb#toolbar=0', 'View - SOURCE', 'width=680,height=600,scrollbars=yes,resizable=yes')">Ver Codigo</a>
+</td>
+ </tr>
+ <tr>
+  <td>671309206</td>
+  <td>17</td>
+  <td>A</td>
+  <td>Java</td>
+  <td class="text-danger"><a class="text-danger" href="" onClick="window.open('../filewindow.php?oid=16824&filename=M3o2NnRMZERQVUo1WDdMQ0krNDJNVng5SkZmSENBdmtjZXF2cTI3K0tXMU1vdjlrVVVrVm5ta1IxQ09ZcVcyZDBCY01yTm5CWUNBSkVqY0NuWTFHQnc9PQ%3D%3D&check=936eaff42683f4e1fa74703c7514f6e3c752617142ee165aafc94ecf20b7c84a#toolbar=0', 'View - OUTUSER','width=680,height=600,scrollbars=yes,resizable=yes')">NO - Wrong answer</a></td>
+<td><a href="../filedownload.php?oid=16821&filename=Kzd3OXRUWUZNZldpVzJPYTIydUtpbkRFZ3RIaENOdGRTMDJSUndtYTNsWFY1bDJIK25FTEwxTmR5b2JxeDFEcFp4UVBXdExOZWhuSlJHQ09XUEowMDQ4eHoxaTIrVUFFVjFNOWRwY2NOV3M9&check=9c44af9a3ced47d4f27a892ba5c22f712ba1faf5842cc30ac87d975fee08b500#toolbar=0">print.java</a>&nbsp;&nbsp;<a href="#" class="btn btn-primary" style="font-weight:bold" onClick="window.open('../filewindow.php?oid=16821&filename=Uks5NTJUUE1mNHIyQUcvRDNaWU5tLzhIS1ZwNk1NZEpjZjFGU3dwMnpBbDl2WExKRjU5Z0NpVDlERnNYQ0QrTXdpRjBXbHozMjdETHUzU2ttaDNkbmRpVWRyYytEWkFkb0NiRWJ3ZWkvNkk9&check=9c44af9a3ced47d4f27a892ba5c22f712ba1faf5842cc30ac87d975fee08b500#toolbar=0', 'View - SOURCE', 'width=680,height=600,scrollbars=yes,resizable=yes')">Ver Codigo</a>
 </td>
  </tr>
 </tbody>
@@ -35,8 +80,6 @@
                <select name="problem" onclick="Arquivo()">
 <option selected value="-1"> -- </option>
 <option value="1">A</option>
-<option value="2">B</option>
-<option value="3">C</option>
 	</select>
            </div>
        </div>
--- src/private/scoretmp/contest-1-team-fabian.php
+++ src/private/scoretmp/contest-1-team-fabian.php
@@ -1,4 +1,4 @@
-<!-- 1632361463 --> <?php exit; ?>
+<!-- 1633967179 --> <?php exit; ?>
 <script language="JavaScript" src="/salomon/hide.js"></script>
 <br>
 <table id="myscoretable" class="table table-bordered table-dark table-hover">
@@ -7,13 +7,13 @@
   <th scope="col">#</th>
   <th scope="col">Usuario</th>
   <th scope="col">Nombre</th>
-<th scope="col" nowrap><b>A &nbsp;</b></th><th scope="col" nowrap><b>B &nbsp;</b></th><th scope="col" nowrap><b>C &nbsp;</b></th><th scope="col">Total</th>
+<th scope="col" nowrap><b>A &nbsp;</b></th><th scope="col">Total</th>
 </tr>
 </thead>
 <tbody>
  <tr class="sitegroup1"><td>1st</td>
-  <td nowrap>fabian</td><td>fabian sierra acarpi</td><td nowrap><img alt="lechuga:" width="18" src="/salomon/balloons/09f7dde6e1d62020b2a6420ea7f8dcc0.png" /><font size="-2">1/67</font>
-</td><td nowrap>&nbsp;&nbsp;</td><td nowrap>&nbsp;&nbsp;</td>  <td nowrap>1 (67min)</td>
+  <td nowrap>fabian</td><td>fabian sierra acarapi</td><td nowrap><img alt="rojo:" width="18" src="/salomon/balloons/0b317cbe1e5d0b6ab93bfa1ab4742302.png" /><font size="-2">1/3</font>
+</td>  <td nowrap>1 (3min)</td>
  </tr>
 </tbody>
 </table>
